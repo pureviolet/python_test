bf72e7e85b3ff93fb196f8ce7e3e1f7cffff3813
Brian C. Christensen <brian@pureviolet.net>
2013-05-20 23:00:51 -0600

    BrianC - Python Test Solutions

--- palinodrome_product.py
+++ palinodrome_product.py
@@ -1,3 +1,22 @@
 """A palindromic number reads the same both ways. The largest palindrome made
 from the product of two 2-digit numbers is 9009 = 91 99.Find the largest
 palindrome made from the product of two 3-digit numbers."""
+
+highest = 0  # highest palindrome product found so far
+xa = xb = 0
+for a in xrange(999, 99, -1):  # start with the highest products
+    for b in range(a, 99, -1):  
+        product = a * b
+        if product <= highest:  # none of the other b's can be high enough
+            break 
+        digits = str(product)
+        back = "".join(reversed(digits))  # reverse the digits
+        if digits == back:
+            highest = product  # we already tested that the product is higher
+            xa = a
+            xb = b
+#            print a, b, digits
+            break
+print "The largest palindrome made from the product of two 3-digit numbers:"
+print xa, xb, highest
+
--- series_product.py
+++ series_product.py
@@ -1,3 +1,31 @@
 """Complete this program to find the greatest produdct of five consecutive
 digits in the list"""
+
+import operator
+
 in_file = open('array.txt')
+lines = in_file.readlines()
+in_file.close()
+
+# assumes that consective digits can span lines
+digits = "".join([ line.strip() for line in lines ])  # remove new lines
+numbers = [ int(x) for x in digits ]  # place all digits in one list
+
+greatest_product = 0  # the maximum product found so far
+index_of_greatest = 0
+i = 0
+end = len(numbers)-5  # starting position of the last string
+while i <= end:
+    five_numbers = numbers[i:i+5]
+    if five_numbers[-1] == 0:  # skip most of the zero products
+        i += 5
+        continue
+    current_product = reduce(operator.mul, five_numbers)  # product
+    if current_product > greatest_product:
+        greatest_product = current_product
+        index_of_greatest = i
+    i += 1
+
+print "greatest product:", greatest_product
+print "calculated from digits at index:", index_of_greatest
+print "these are the digits:", "".join([ str(x) for x in numbers[index_of_greatest:index_of_greatest+5]])
